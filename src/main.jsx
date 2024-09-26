e0272454c775bcd3b688e2eea2ea9207e7c4f296
Yogesh Gupta <Yogesh798714@gmial.com>
2024-09-26 22:49:44 +0530

    Updated Gallery.jsx with correct image paths

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
+  <Router basename="/King-Sukh-Guest-House/">
     <AuthProvider>
       <App />
     </AuthProvider>
